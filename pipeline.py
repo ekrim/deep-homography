09eb12d896709880042f1db7d3422dcd33c3228d
Evan Kriminger <evankriminger@gmail.com>
2018-07-20 20:15:46 -0700

    dataloader working now

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,8 @@ import pickle
 import PIL
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
@@ -36,13 +38,20 @@ class HomographyDataset(Dataset):
    
     label_str = self.num_and_label[idx][1] 
     label = np.array([float(el) for el in label_str.split(',')]).astype(np.float32)
-    sample = {'image': img, 'label': label}
+    sample = {
+      'image': torch.from_numpy(img.transpose((2,0,1))),
+      'label': torch.from_numpy(label)}
     return sample
 
 
 if __name__ == '__main__':
   dataset = HomographyDataset()
-  for i in range(len(dataset)):
-    sample = dataset[i]
-    print(sample['image'].shape)
-    print(sample['label'].shape)
+  dataloader = DataLoader(
+    dataset,
+    batch_size=4,
+    shuffle=True,
+    num_workers=4)
+
+  for i_batch, sample_batch in enumerate(dataloader):
+    print(sample_batch['image'].size())
+    print(sample_batch['label'].size())
