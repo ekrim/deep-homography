924002be4b28500d24a3172c7d29136d750f8235
Evan Kriminger <evankriminger@gmail.com>
2018-07-28 03:31:13 -0700

    still a problem with preds all being the same

--- main.py
+++ main.py
@@ -12,58 +12,18 @@ from models import Net
 from pipeline import HomographyDataset 
 
 
-def train(dataloader_train, dataloader_eval, epochs, lr):
-  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-  print(device)
-
-  net = Net()
-  net.to(device)
-  criterion = nn.MSELoss()
-  optimizer = optim.Adam(net.parameters(), lr=lr)
-
-  log_every = len(dataloader_train)//2
- 
-  for epoch in range(epochs):
-    for i, data in enumerate(dataloader_train):
-      optimizer.zero_grad()
-      inputs, labels = data['image'].to(device), data['label'].to(device)
-   
-      outputs = net(inputs)
-      loss = criterion(outputs, labels)
-      loss.backward()
-      optimizer.step()
-     
-      if i % log_every == log_every-1:
-        running_loss = 0.0
-        for data_eval in dataloader_eval:
-          inputs, labels = data_eval['image'].to(device), data_eval['label'].to(device)
-          outputs = net(inputs)
-          loss = criterion(outputs, labels)
-          running_loss += loss.item()
-        running_loss /= len(dataloader_eval)
-        print('epoch {:d}, batch {:d}: loss {:0.3f}'.format(epoch, i, running_loss))
-        print('true labels')
-        print(labels.detach().cpu().numpy()[:3])
-        print('predicted labels')
-        print(outputs.detach().cpu().numpy()[:3])
-        print('images')
-        print(inputs.detach().cpu().numpy()[:3, 0, :3, :3])
-
-
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--epochs', type=int, default=2)
-  parser.add_argument('--batch_size', type=int, default=64)
-  parser.add_argument('--val_frac', type=float, default=0.05)
-  parser.add_argument('--lr', type=float, default=0.01)
-  args = parser.parse_args(sys.argv[1:])
+def main(args):
   epochs = args.epochs
   batch_size = args.batch_size
   val_frac = args.val_frac
   lr = args.lr
-  
+
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+  print(device)
+
   dataset_train = HomographyDataset(val_frac=val_frac, mode='train')
   dataset_eval = HomographyDataset(val_frac=val_frac, mode='eval')
+
   dataloader_train= DataLoader(
     dataset_train,
     batch_size=batch_size,
@@ -76,4 +36,56 @@ if __name__ == '__main__':
     shuffle=False,
     num_workers=2)
 
-  train(dataloader_train, dataloader_eval, epochs, lr)
+  net = Net()
+  net.to(device)
+  criterion = nn.MSELoss()
+  optimizer = optim.Adam(net.parameters(), lr=lr)
+
+  log_every = len(dataloader_train)//2
+ 
+  for epoch in range(epochs):
+    train(dataloader_train, device, net, criterion, optimizer)
+    test(dataloader_eval, device, net, criterion, epoch)
+
+
+def train(dataloader_train, device, net, criterion, optimizer):
+  net.train()
+  for i, data in enumerate(dataloader_train):
+    optimizer.zero_grad()
+    inputs, labels = data['image'].to(device), data['label'].to(device)
+   
+    outputs = net(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+
+
+def test(dataloader_eval, device, net, criterion, epoch):
+  net.eval()
+  with torch.no_grad():
+
+    running_loss = 0.0
+    for data_eval in dataloader_eval:
+      inputs, labels = data_eval['image'].to(device), data_eval['label'].to(device)
+      outputs = net(inputs)
+      loss = criterion(outputs, labels)
+      running_loss += loss.item()
+    running_loss /= len(dataloader_eval)
+  
+    print('epoch {:d}:  {:0.4f}'.format(epoch, running_loss))
+    print('true labels')
+    print(labels.detach().cpu().numpy()[:3])
+    print('predicted labels')
+    print(outputs.detach().cpu().numpy()[:3])
+    print('images')
+    print(inputs.detach().cpu().numpy()[:3, 0, :3, :3])
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--epochs', type=int, default=2)
+  parser.add_argument('--batch_size', type=int, default=64)
+  parser.add_argument('--val_frac', type=float, default=0.05)
+  parser.add_argument('--lr', type=float, default=0.01)
+  args = parser.parse_args(sys.argv[1:])
+  main(args) 
