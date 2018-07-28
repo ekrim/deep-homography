579343512566c3b561d7afe4c04abcd325507c1f
Evan Kriminger <evankriminger@gmail.com>
2018-07-28 13:32:24 -0700

    batchnorm

--- models.py
+++ models.py
@@ -9,24 +9,24 @@ class Net(nn.Module):
     super().__init__()
     self.conv1 = nn.Conv2d(2, 16, 5)
     self.pool1 = nn.AvgPool2d(4)
+    self.bn1 = nn.BatchNorm2d(16)
     self.conv2 = nn.Conv2d(16, 32, 4)
     self.pool2 = nn.AvgPool2d(3)
+    self.bn2 = nn.BatchNorm2d(32)
     self.conv3 = nn.Conv2d(32, 1, 3)
     self.fc1 = nn.Linear(1*7*7, 64)
     self.fc2 = nn.Linear(64, 64)
-    self.fc3 = nn.Linear(64, 64)
-    self.fc4 = nn.Linear(64, 64)
     self.fc5 = nn.Linear(64, 8)
     
   def forward(self, x):
     x = self.pool1(F.relu(self.conv1(x)))
+    x = self.bn1(x)
     x = self.pool2(F.relu(self.conv2(x)))
+    x = self.bn2(x)
     x = F.relu(self.conv3(x))
     x = x.view(-1, 1*7*7)
     x = F.relu(self.fc1(x)) 
     x = F.relu(self.fc2(x)) 
-    x = F.relu(self.fc3(x)) 
-    x = F.relu(self.fc4(x)) 
     x = F.tanh(self.fc5(x))
     return x
 
