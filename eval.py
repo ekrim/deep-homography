d5d05ea33a4ea34643cd40bc798da690b70b6354
Evan Kriminger <evankriminger@gmail.com>
2018-08-05 21:39:04 -0700

    ok, running on train data nicely

--- eval.py
+++ eval.py
@@ -14,10 +14,11 @@ def normalize_batch(image_batch):
   return torch.from_numpy(image_batch)
 
 
-def read_and_convert(f, edges):
+def read_and_convert(f, edges=None):
   big_img = cv2.imread(f)
   assert big_img is not None, 'no such file'
-  big_img = big_img[edges[0]:edges[1], edges[2]:edges[3]]
+  if edges is not None:
+    big_img = big_img[edges[0]:edges[1], edges[2]:edges[3]]
   img = cv2.cvtColor(big_img, cv2.COLOR_BGR2GRAY)
   return big_img, img
 
@@ -31,9 +32,10 @@ def prep_batch(f1, f2, edges, n_patches, big_patch, patch_size):
  
   row_center = img1.shape[0]//2, img1.shape[1]//2
   for i in range(n_patches):
-     
-    delta_row = np.random.randint(0, img1.shape[0] - big_patch)
-    delta_col = np.random.randint(0, img1.shape[1] - big_patch)
+    delta_row = np.random.randint(0, max(img1.shape[0] - big_patch, 1))
+    delta_col = np.random.randint(0, max(img1.shape[1] - big_patch, 1))
+    delta_row = (img1.shape[0] - big_patch)//2
+    delta_col = (img1.shape[1] - big_patch)//2
     patch1 = img1[delta_row:delta_row+big_patch, delta_col:delta_col+big_patch]
     patch2 = img2[delta_row:delta_row+big_patch, delta_col:delta_col+big_patch]
 
@@ -59,22 +61,24 @@ def pred_homography(batch, patch_size, model_file='homography_model.pytorch', sc
     pts1 = np.float32([0, 0, patch_size, 0, patch_size, patch_size, 0, patch_size]).reshape(-1,1,2)
     pts2 = mean_shift.reshape(-1,1,2) + pts1
    
-    #h = np.linalg.inv(cv2.findHomography(pts1, pts2)[0])
-    h = cv2.findHomography(pts1, pts2)[0]
+    h = np.linalg.inv(cv2.findHomography(pts1, pts2)[0])
+    #h = cv2.findHomography(pts1, pts2)[0]
     
     return h
 
 
 if __name__ == '__main__':
   f1, f2 = 'house_1.jpg', 'house_3.jpg' 
+  f1, f2 = 'tmp/000000001_orig.jpg', 'tmp/000000001_warp.jpg'
   edges = [1200, 2600, 300, 3000]
-  n_patches = 20
+  edges = None
+  n_patches = 1
   patch_size = 128
-  big_patch = 9*patch_size
+  big_patch = patch_size
 
   img1, img2, batch = prep_batch(f1, f2, edges, n_patches, big_patch, patch_size)
   h = pred_homography(batch, patch_size)
-  new_img = cv2.warpPerspective(img1, h, (3000, 1400))
+  new_img = cv2.warpPerspective(img1, h, (128, 128))
   plt.figure(1)
   plt.imshow(img1[:,:,[2,1,0]])
   plt.figure(2)
